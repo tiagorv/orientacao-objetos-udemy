b3db14009865a5506109f5e1df815e60318410f0
tiagorv <tiago_valerio_betha@hotmail.com>
2019-07-22 06:10:16 -0300

    Classes tabuleiro e peca

---
+++ src/secao11/tabuleiro/Peca.java
@@ -0,0 +1,16 @@
+package secao11.tabuleiro;
+
+public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+	
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+}
