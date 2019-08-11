e4c47936c4e5c4a676bdf115cd200eab1095eb93
tiagorv <tiago_valerio_betha@hotmail.com>
2019-08-11 19:07:07 -0300

    Leitura de Arquivo

---
+++ src/secao12_aula157/LerArquivo.java
@@ -0,0 +1,26 @@
+package secao12_aula157;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LerArquivo {
+	public static void main(String[] args) {
+		
+		File meuArquivo = new File("c:\\tiago\\arquivo.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(meuArquivo);
+			while (sc.hasNext()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Erro leitura de arquivo: ");
+			e.printStackTrace();
+		}finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+}
